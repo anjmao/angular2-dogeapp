c30e1d98ae564a015960cf09f1866874794d9c99
Andžej Maciusovič <andzej.maciusovic@visma.com>
2015-11-12 16:31:35 +0200

    playing with mocha tests setup, need cleanup

---
+++ server/tests/doge-friends-service-tests.ts
@@ -0,0 +1,16 @@
+﻿/// <reference path="../typings/chai/chai.d.ts" />
+
+import assertsImport = require('chai');
+var assert = assertsImport.assert;
+
+describe('Array', () => {
+
+    describe('#indexOf()', () => {
+
+        it('should return -1 when the value is not present', () => {
+            assert.equal(-1, [1, 2, 3].indexOf(5));
+            assert.equal(-1, [1, 2, 3].indexOf(0));
+        });
+
+    });
+});
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,1 +1,2 @@
 /// <reference path="cors/cors.d.ts" />
+/// <reference path="mocha/mocha.d.ts" />
